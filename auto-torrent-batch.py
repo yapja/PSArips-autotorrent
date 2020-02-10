168e9a49cef94459c68a298d9265afac1b6a5248
yapja <yap.antoni@gmail.com>
2020-02-10 20:14:41 +0800

    Add files via upload

---
+++ auto-torrent-batch.py
@@ -0,0 +1,58 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
+import os
+
+tv_series = input("Enter TV Series: ")
+
+profile = webdriver.FirefoxProfile()
+profile.set_preference('browser.download.folderList', 2)
+profile.set_preference('browser.download.manager.showWhenStarting', False)
+profile.set_preference('browser.download.dir', 'E:\\')
+profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'application/x-bittorrent')
+
+# Starts headless Firefox and redirect to PSArips page
+# service_log_path disables geckodriver logging
+options = Options()
+# options.headless = True
+driver = webdriver.Firefox(options = options, service_log_path='NUL', firefox_profile=profile)
+
+# Gets current working directory
+path = os.getcwd() + '\\'
+
+# Remember to include .xpi at the end of your file names 
+extensions = [
+    '{529b261b-df0b-4e3b-bf42-07b462da0ee8}.xpi',
+    'uBlock0@raymondhill.net.xpi'
+    ]
+
+# Installs extensions needed (uBlock Origin, Universal Bypass)
+for extension in extensions:
+    driver.install_addon(path + extension, temporary=True)
+
+driver.get('https://psarips.one')
+driver.find_element_by_xpath('/html/body/div/div/div/div/div/div/div/div[2]/form/label/input').send_keys(tv_series)
+driver.find_element_by_xpath('/html/body/div/div/div/div/div/div/div/div[2]/form/input').click()
+driver.find_element_by_xpath('/html/body/div/div/div/div/div/section/div[2]/div[2]/div[1]/article[1]/div/div[1]/h2').click()
+
+try:
+    divs = driver.find_elements_by_css_selector('div[class="sp-head"]')
+    links = []
+    for div in divs:
+        title = div.text.lower()
+        if (title.find('1080p') > -1):
+          div.click()
+          text = driver.find_element_by_link_text('TORRENT')
+          links.append(text.get_attribute('href'))
+          div.click()
+
+    for link in links:
+        driver.get(link)
+        driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/main/article/div/div/p[2]/a').click()
+
+except Exception as e:
+    print(e)
+
+driver.quit()
+
+input('Press any key to exit!')
